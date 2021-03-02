1ec85104bc763ea661f9bf3057d674bb06f5ba27
Valéry <valerygarrec@gmail.com>
2021-03-03 00:08:57 +0100

    Mes dernieres sauvegardes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,8 @@
 package com.company;
 
+import java.text.DateFormat;
 import java.util.Scanner;
+import java.util.TimeZone;
 
 public class Main {
 
@@ -8,36 +10,38 @@ public class Main {
         // On déclare un scanner pour lire les entrées de la console
         Scanner sc = new Scanner(System.in);
 
-        // On affiche la date et l'heure du ticket
-
-
-        // On affiche le nombre de couverts
-        System.out.println("Nombre de couverts");
-        int nombreCouvert = sc.nextInt ();
+        //On affiche le plat commandé
+        System.out.println("Plat # : ");
+        String typePlat = sc.nextLine();
         sc.nextLine();
 
-        // On affiche la quantité du plat commandé
-        System.out.println("Quel est le nombre de plat");
-        int quantitePlat = sc.nextInt ();
+        // On affiche la quantité
+        System.out.println("Quantité");
+        int quantitePlat = sc.nextInt();
         sc.nextLine();
 
-        //On affiche le type de plat consommé
-        System.out.println("Entrez votre plat : ");
-        sc.nextLine();
-
-        // On affiche le prix unitaire pour chaque ligne
-        System.out.println("Entrez le prix unitaire");
+        // On affiche le prix unitaire de chaque plat
+        System.out.println("Prix unitaire");
         float prixUnitaire = sc.nextFloat();
         sc.nextLine();
 
         // On affiche le prix global pour chaque ligne
         float montantTotal = (quantitePlat * prixUnitaire);
-        System.out.println("Montant total à régler " + montantTotal);
+        System.out.println("Total = " + montantTotal + "€");
+
+        if (quantitePlat <= 0) {
+            System.out.println("Pas de commande");
+        } else ;
+        {
+            System.out.println(quantitePlat + " " +typePlat + " " + prixUnitaire + "€");
+        }
 
         // On affiche un message de remerciement "Merci de votre visite & à bientôt"
         System.out.println("Merci de votre visite et à bientôt !");
         sc.nextLine();
         // On ferme le scanner
         sc.close();
+
     }
 }
+
